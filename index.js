1fbc84154bbb4d17f5ba938862e841e4be22c289
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-11-29 14:17:18 -0500

    added in search functionality for todo's

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let todoList = [];
 let tempval;
+let newstring;
 
 function Todo(title, content) {
     this.title = title;
@@ -68,9 +69,26 @@ app.post("/postedit", (req, res) => {
 
 });
 
+// Route functionality for checking if todo is available iterates through todolist to find the right title and then index that is used for loading the proper todo.
+app.post("/find-todo", (req, res) => {
+    newstring = req.body.string;
+    function checkstring(newstring) {
+        for (i = 0; i < todoList.length; i++) {
+            if (todoList[i].title === newstring) {
+                return i;
+            } else {
+                return res.render("failToFind.ejs", { todoList: todoList, tempval: tempval });
+            }
+        }
+    }
+    console.log(checkstring(newstring));
+    tempval = checkstring(newstring);
+    res.render("content.ejs", { todoList: todoList, tempval: tempval });
+});
+
 // route with functionality for deleting todos, just splices temp data pushed in array made from the create-todo route
 app.post("/delete-todo", (req, res) => {
-    todoList.splice(tempval,1);
+    todoList.splice(tempval, 1);
     res.render("index.ejs", { todoList: todoList, tempval: tempval });
 
 });
