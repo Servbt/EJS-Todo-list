7a5b2bdd9fb4e99c873be326ba93de439aa2b220
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-29 02:22:11 -0500

    samepage delete header fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { response } from "express";
 import bodyParser from "body-parser";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
